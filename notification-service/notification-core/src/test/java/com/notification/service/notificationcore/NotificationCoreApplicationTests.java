c0b3b9e0240f4229280d68febd0b1104e75eb612
nafeyah <nafeyahthenx@gmail.com>
2023-02-19 15:05:38 +0530

    initial commit

---
+++ notification-service/notification-core/src/test/java/com/notification/service/notificationcore/NotificationCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification.service.notificationcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
